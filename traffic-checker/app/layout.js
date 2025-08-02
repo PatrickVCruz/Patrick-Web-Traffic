e75d3220a2fcd22d33f35f878bdfa7be181653d2
Patrick <patrickvcruz@yahoo.com>
2025-08-02 19:56:19 +0100

    feat(front-end): fix footer

--- traffic-checker/app/layout.js
+++ traffic-checker/app/layout.js
@@ -31,10 +31,8 @@ export default function RootLayout({ children }) {
         </main>
 
         <footer className="bg-gray-800 text-white py-4">
-            <div className="max-w-7xl mx-auto px-4 text-center">
-                <p>
-                    &copy; 2025 Patrick Victor Cruz.
-                </p>
+            <div className="max-w-7xl mx-auto text-center">
+                <p>&copy; 2025 Patrick Victor Cruz.</p>
             </div>
         </footer>
         </body>
