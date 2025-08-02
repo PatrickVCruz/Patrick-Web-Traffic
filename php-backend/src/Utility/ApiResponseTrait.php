90d032ddd8209afad4f6e65c31afe9ea938544a8
Patrick Cruz <patcruzer1204@yahoo.com>
2025-08-02 11:35:19 +0100

    feat(php-backend): uses response codes

--- php-backend/src/Utility/ApiResponseTrait.php
+++ php-backend/src/Utility/ApiResponseTrait.php
@@ -3,13 +3,10 @@
 namespace App\Utility;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 trait ApiResponseTrait
 {
-    private const HTTP_UNAUTHORIZED = 401;
-    private const HTTP_UNPROCESSABLE_ENTITY = 422;
-    private const HTTP_NOT_FOUND = 404;
-    private const HTTP_CREATED = 201;
     protected int $statusCode = 200;
 
     public function getStatusCode(): int
@@ -38,21 +35,21 @@ trait ApiResponseTrait
 
     public function respondUnauthorized(string $message = 'Not authorized!'): JsonResponse
     {
-        return $this->setStatusCode(self::HTTP_UNAUTHORIZED)->respondWithErrors($message);
+        return $this->setStatusCode(Response::HTTP_UNAUTHORIZED)->respondWithErrors($message);
     }
 
     public function respondValidationError(string $message = 'Validation errors'): JsonResponse
     {
-        return $this->setStatusCode(self::HTTP_UNPROCESSABLE_ENTITY)->respondWithErrors($message);
+        return $this->setStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY)->respondWithErrors($message);
     }
 
     public function respondNotFound(string $errorMessage = 'Not found!'): JsonResponse
     {
-        return $this->setStatusCode(self::HTTP_NOT_FOUND)->respondWithErrors($errorMessage);
+        return $this->setStatusCode(Response::HTTP_NOT_FOUND)->respondWithErrors($errorMessage);
     }
 
     public function respondCreated(array $data = []): JsonResponse
     {
-        return $this->setStatusCode(self::HTTP_CREATED)->respond($data);
+        return $this->setStatusCode(Response::HTTP_CREATED)->respond($data);
     }
 }
